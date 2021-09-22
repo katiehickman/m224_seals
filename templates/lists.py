86487068368c8c76091318b16457a5d6315c17d3
katiehickman <katie.hickman2704@gmail.com>
2021-09-22 12:34:25 -0700

    tpt5 1 and 2

---
+++ templates/lists.py
@@ -0,0 +1,25 @@
+var1 = 3
+var2: str = "Mr. Mortensen"
+var3: chr = 'f'
+var4: float = 0.4
+
+print(var1 + ': Integer')
+print(var2 + ': String')
+print(var3 + ': Character')
+print(var4 + ': Float')
+
+list1 = [5, 3, 4, 1, 2]
+list1[3] = 1
+list1[4] = 2
+list1[1] = 3
+list1[2] = 4
+list1[0] = 5
+list2 = list1
+print(list2)
+
+
+
+
+
+
+

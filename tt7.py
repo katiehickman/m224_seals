35c1d01637b7d93c930fa89c91a83cdc287ab4a8
katiehickman <katie.hickman2704@gmail.com>
2021-10-07 11:39:01 -0700

    tt7 activity 1 katie

---
+++ tt7.py
@@ -0,0 +1,18 @@
+# Activity 1 Katie
+student_list = ['pam','rob','joe','greg','bob','amy','matt']
+print(student_list[2:5])
+print(student_list[:-5])
+print(student_list[5:])
+print(student_list[:])
+if 'rob' in student_list:
+    print("Yes, rob is in the student_list")
+
+# Activity 2 Katie
+p1 = { "name":"John", "age":61, "city":"Eugene"}
+p2 = { "name":"Risa", "age":16, "city":"New York"}
+p3 = { "name":"Ryan", "age":16, "city":"Los Angeles"}
+p4 = { "name":"Shekar", "age":16, "city":"San Francisco"}
+# a list of dictionaries
+list_of_people = [p1, p2, p3, p4]
+# write some code to Print List of people one by one
+print(list_of_people)
